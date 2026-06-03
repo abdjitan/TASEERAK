04ee865fb67fc48a76baf87a047044125f69bbd3
abdjitan <ABDjitanj@gmail.com>
2026-06-03 13:15:47 +0300

    configure xlsx external package, ignore eslint in builds

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: { allowedOrigins: ['localhost:3000'] },
+    serverActions: { allowedOrigins: ['localhost:3000', 'taseerak.vercel.app'] },
+    serverComponentsExternalPackages: ['xlsx'],
   },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '*.supabase.co' },
     ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 }
 
